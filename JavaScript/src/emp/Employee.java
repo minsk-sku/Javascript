fb47853cf77db6ac1319ca9461bc2d8a595700e1
User <User@YD03-01>
2019-10-23 18:01:31 +0900

    1023

---
+++ JavaScript/src/emp/Employee.java
@@ -0,0 +1,70 @@
+package emp;
+
+public class Employee {
+	private int employeeId;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String hireDate;
+	private int salary;
+	private String jobId;
+	private String deptName;
+	
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstname;
+	}
+	public void setFirstName(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastName() {
+		return lastname;
+	}
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstname=" + firstname + ", lastname=" + lastname + ", email="
+				+ email + ", hireDate=" + hireDate + ", salary=" + salary + ", jobId=" + jobId + ", deptName="
+				+ deptName + "]";
+	}
+	
+	
+
+}
